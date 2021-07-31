ded4ba5471e4b0e0ae5572c6f1a78cab93816ac7
Kaydannik <kaydannik.a@gmail.com>
2021-07-31 20:33:34 +0100

    Fix audio duration for Iphone
    * Make token being parameter from commandline
      Run it like python3 main.py hereisyourtoken
    
    * Fixed path to files
    * Fixed filenames

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import telebot
 import youtube_dl
 import os
-bot = telebot.TeleBot("token", parse_mode=None)
+import sys
+import re
+bot = telebot.TeleBot(sys.argv[1], parse_mode=None)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -14,23 +16,31 @@ def send_welcome(message):
     sent = bot.send_message(message.chat.id, "send me your link)")
     bot.register_next_step_handler(sent, task)
 
+def get_valid_filename(s):
+    s = str(s).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', s)
 
 def task(link):
     video_url = link.text
     video_info = youtube_dl.YoutubeDL().extract_info(
         url=video_url, download=False
     )
-    filename = f"{video_info['title']}.mp3"
+    filename_tmp = f"{get_valid_filename(video_info['title'])}.dat"
+    filename =  f"{get_valid_filename(video_info['title'])}.mp3"
     options = {
         'format': 'bestaudio/best',
         'keepvideo': False,
-        'outtmpl': filename,
+        'outtmpl': filename_tmp,
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
     }
-
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
-    audio = open('C:\\Users\\PC\\PycharmProjects\\myfirstbot\\' + format(filename), 'rb')
-
+    audio = open(filename, 'rb')
+    
     # Grab known info from metadata
     duration = video_info["duration"]
     title = video_info.get("track", None)
@@ -38,7 +48,7 @@ def task(link):
     # Send the audio in response
     bot.send_audio(link.chat.id, audio, duration=duration, title=title, performer=artist)
     audio.close()
-    os.remove("C:\\Users\\PC\\PycharmProjects\\myfirstbot\\" + format(filename))
+    os.remove(filename)
 
 
 bot.polling(none_stop=False, interval=0, timeout=20)
